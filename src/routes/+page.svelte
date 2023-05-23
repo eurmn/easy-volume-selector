d30f90da68d48e052d2957f5894f91c274fe11e4
Lucas <lucas.garrafielo@academico.ufpb.br>
2023-05-23 13:27:14 -0300

    Adapter Static

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { scale } from 'svelte/transition';
 
 	let position: GeolocationPosition;
+    let error: string | undefined;
 
 	onMount(() => {
-	  navigator.geolocation.watchPosition((p) => (position = p), null, {
+	  navigator.geolocation.watchPosition(
+	    (p) => (position = p),
+	    (e) => error = e.message, {
 	    enableHighAccuracy: true,
 	    timeout: 5000,
 	    maximumAge: 0
@@ -45,8 +49,15 @@
             </div>
             <div>{Math.abs(Math.floor((position.coords.longitude + position.coords.latitude) * 100)/100)}%</div>
         </div>
+    {:else if error}
+        <div
+            class="w-full flex items-center mt-5 gap-2 overflow-hidden text-red-7 font-semibold">
+            <div class="i-heroicons:x-circle-20-solid text-5xl"></div>
+            <div>Error when fetching your location: {error}</div>
+        </div>
     {:else}
-        <div class="w-full flex justify-center items-center mt-5 gap-2 flex-col">
+        <div
+            class="w-full flex justify-center items-center mt-5 gap-2 flex-col">
             <div class="i-svg-spinners:3-dots-rotate text-5xl text-zinc-8"></div>
             <div>Getting your location</div>
         </div>
